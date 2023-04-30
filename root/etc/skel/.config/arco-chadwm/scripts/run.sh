053257610611fdee6e23984c7c75ea303529b3ca
arcolinuxz <arcolinuxinfo@gmail.com>
2023-04-30 06:46:13 +0200

    new wallpaper

--- root/etc/skel/.config/arco-chadwm/scripts/run.sh
+++ root/etc/skel/.config/arco-chadwm/scripts/run.sh
@@ -23,22 +23,23 @@ run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 
 run "nm-applet"
 run "pamac-tray"
-run "variety"
+#run "variety"
 run "xfce4-power-manager"
 run "blueberry-tray"
 run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 picom -b  --config ~/.config/arco-chadwm/picom/picom.conf &
 run "numlockx on"
-#run "volumeicon"
-run "pa-applet"
+run "volumeicon"
+#run "pa-applet"
 sxhkd -c ~/.config/arco-chadwm/sxhkd/sxhkdrc &
 #run "nitrogen --restore"
 run "conky -c $HOME/.config/arco-chadwm/conky/system-overview"
 #you can set wallpapers in themes as well
 #feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
 #feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-feh --bg-fill ~/.config/arco-chadwm/wallpaper/chadwm.jpg &
+#feh --bg-fill ~/.config/arco-chadwm/wallpaper/chadwm.jpg &
+feh --bg-fill ~/.config/arco-chadwm/wallpaper/chadwm1.png &
 #feh --randomize --bg-fill /home/erik/Insync/Apps/Wallhaven/*
 
 
