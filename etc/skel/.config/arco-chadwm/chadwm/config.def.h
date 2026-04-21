9f887c9a2e3bda05c885124b209ddc9cfe342432
Erik Dubois <erik.dubois@gmail.com>
2026-04-21 06:26:12 +0200

    update

--- etc/skel/.config/arco-chadwm/chadwm/config.def.h
+++ etc/skel/.config/arco-chadwm/chadwm/config.def.h
@@ -85,8 +85,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static char *tags[] = { "", "", "", "", "", "", "", "", "", "" };
-//static char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+//static char *tags[] = { "", "", "", "", "", "", "", "", "", "" };
+static char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
 //static char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
 //static char *tags[] = { "", "", "", "", "", "", "", "", "", "" };
 //static char *tags[] = { "Web", "Chat", "Edit", "Meld", "Vb", "Mail", "Video", "Image", "Files", "Music" };
